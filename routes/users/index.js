e009d7cf16327883eb23672753a154f4bbf15413
David Higginbotham <davehigginbotham@gmail.com>
2013-12-25 03:26:39 -0700

    users route working

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = module.exports = express();
 var cfg = require('../../config/app');
+var moment = require('moment');
 
 app.set('views', cfg.templateDir);
 app.set('view engine', cfg.viewEngine);
 
 var middle = require('./middle');
 
-var prefix = '/auth';
+var prefix = '/users';
 var id = '/:id?';
 
 var back = function (req, res) {
@@ -15,5 +16,7 @@ var back = function (req, res) {
 };
 
 app.get(prefix, middle.collection, function (req, res) {
-  return res.render('users/view');
+  return res.render('users/view', {
+    moment: moment
+  });
 });
