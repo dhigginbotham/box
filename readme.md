7a75eba7f06fc40efe9a7a1a7b97a48159b47a1e
David Higginbotham <davehigginbotham@gmail.com>
2013-12-25 03:06:47 -0700

    readme

--- readme.md
+++ readme.md
@@ -1,4 +1,2 @@
 ## box
-`box` is an `express` *boilerplate*, it doesn't care what you do with it. it's just there to help you do some basic stuff then move on further.
-
-### under the hood
+`box` is an `express` *boilerplate*, it doesn't care what you do with it. Inside of box it comes with a markdown parser base for wikis, blogs, pages, passport auth, ui from bootstrap and a couple other gems sprinkled in.
--- seed/admin.js
+++ seed/admin.js
@@ -13,16 +13,27 @@ var account = {
 // create default admin seed
 module.exports = function (fn) {
   User.findOne({username: /^admin$/i}, function (err, seed) {
+
     if (err) return fn(err, null);
+
     if (!seed) {
+
       var admin = new User(account);
+
       admin.last_login.push(Date.now());
+
       admin.save(function (err) {
+
         if (err) return fn(err, null);
+
         return fn(null, null);
+        
       });
+
     } else {
+
       return fn(null, null);
+
     }
   });
 };
